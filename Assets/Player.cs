69eae095fb85b989a7ceefba75a4618897966586
Mel Avery Rumsey <melavery6321@gmail.com>
2021-11-24 15:58:45 -0600

    Movement added / Scene elements added

---
+++ Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    CharacterController _controller;
+    public float _speed = 10f;
+
+
+    void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+    }
+
+}
